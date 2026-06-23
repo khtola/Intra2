18be3b0686094947094bd7b1fcb0095ce7f8dbb2
Khun Tola <khuntola@Khuns-MacBook-Pro.local>
2026-06-23 14:25:12 +0700

    test commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,7 +5,6 @@ import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
 export default function RootLayout() {
   if (!publishableKey) {
     throw new Error(
